df7a9678dc131943301b6a44036416f2df0a63af
Figen Daghan <figendaghan06@gmail.com>
2024-02-16 16:04:09 +0300

    project creater added

---
+++ project-creater/src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import ProjectSideBar from './Components/ProjectSideBar';
+import NewProject from './Components/NewProject';
+import NoProjectSelected from './Components/NoProjectSelected';
+
+function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+// 1
+function handleStartAddProject() {
+  setProjectState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId: null,
+    }
+  });
+}
+
+// 2
+function handleAddProject(projectData) {
+  setProjectState(prevState => {
+    const newProject = {
+      ...projectData,
+      id: Math.random(),
+    };
+
+    return {
+      ...prevState,
+      selectedProjectId: undefined,
+      projects: [...prevState.projects, newProject],
+    };
+  });
+}
+
+
+let content;
+
+if(projectState.selectedProjectId === null) {
+   content = <NewProject onAdd={handleAddProject} />
+} else if(projectState.selectedProjectId === undefined) {
+   content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+}
+
+  return (
+    <main className='h-screen my-8 flex gap-8'>
+      <ProjectSideBar
+       onStartAddProject={handleStartAddProject}
+       projects={projectState.projects}
+       />
+      {content}
+      
+    </main>
+  )
+}
+
+export default App;
