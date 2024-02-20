157e23b1a4105ca71337ee73fc2ff013e8828551
Figen Daghan <figendaghan06@gmail.com>
2024-02-20 12:24:21 +0300

    Project creater updated

--- project-creater/src/App.jsx
+++ project-creater/src/App.jsx
@@ -2,12 +2,52 @@ import React, { useState } from 'react';
 import ProjectSideBar from './Components/ProjectSideBar';
 import NewProject from './Components/NewProject';
 import NoProjectSelected from './Components/NoProjectSelected';
+import SelectedProject from './Components/SelectedProject';
 
 function App() {
   const [projectState, setProjectState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   });
+
+  // 6
+  function handleAddTask(text) {
+    setProjectState(prevState => {
+     const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+  
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks],
+      };
+    });
+  }
+
+  // 7
+function handleDeleteTask(id) {
+  setProjectState(prevState => {
+    return {
+      ...prevState,
+      tasks: prevState.tasks.filter(
+        (task) => task.id !== id),
+    };
+  });
+}
+
+// 4
+function handleSelectProject(id) {
+  setProjectState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId: id,
+    }
+  });
+}
 
 // 1
 function handleStartAddProject() {
@@ -33,9 +73,10 @@ function handleCancelAddProject() {
 // 2
 function handleAddProject(projectData) {
   setProjectState(prevState => {
+    const projectId = Math.random();
     const newProject = {
       ...projectData,
-      id: Math.random(),
+      id: projectId,
     };
 
     return {
@@ -45,9 +86,41 @@ function handleAddProject(projectData) {
     };
   });
 }
+// 5
+function handleDeleteProject() {
+  setProjectState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId: undefined,
+      projects: prevState.projects.filter(
+        (project) => project.id !== prevState.selectedProjectId
+      ),
+    };
+  });
+}
+// 8 Back
+function handleBackProject() {
+  setProjectState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId: undefined,
+    }
+  });
+}
 
 
-let content;
+const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId);
+
+let content = (
+  <SelectedProject
+ project={selectedProject}
+ onDelete= {handleDeleteProject}
+ onBack= {handleBackProject} //
+ onAddTask={handleAddTask}
+ onDeleteTask={handleDeleteTask}
+ tasks={projectState.tasks}
+ />
+)
 
 if(projectState.selectedProjectId === null) {
    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
@@ -60,7 +133,9 @@ if(projectState.selectedProjectId === null) {
       <ProjectSideBar
        onStartAddProject={handleStartAddProject}
        projects={projectState.projects}
-       />
+       onSelectProject={handleSelectProject}
+       selectedProjectId={projectState.selectedProjectId}
+     />
       {content}
       
     </main>
---
+++ project-creater/src/Components/SelectedProject.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Task from './Task';
+
+function SelectedProject({project, onDelete, onBack, onAddTask, onDeleteTask, tasks}) {
+
+const formattedDate = new Date(project.dueDate).toLocaleDateString('en-Us', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+});
+
+  return (
+    <div className='w-[35rem] mt-16'>
+        <header className="pb-4 mb-4 border-b-2 border-stone-300">
+       <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold text-stone-600 mb-2">
+            {project.title}
+        </h1>
+        {/* new */}
+        <menu className="flex items-center justify-end gap-4 my-4">
+            <li>
+                {/*  */}
+            <button
+         className='text-stone-600 hover:text-stone-950'
+         onClick={onDelete}
+         >
+            Delete
+        </button>
+        {/*  */}
+            </li>
+            <li>
+            <button
+        onClick={onBack}
+        className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
+        >
+            Back
+        </button>
+            </li>
+        </menu>
+       
+       
+       </div>
+       <p className='mb-4 text-stone-400'>{formattedDate}</p>
+       <p className='text-stone-600 whitespace-pre-wrap'>{project.description}</p>
+        </header>
+        <Task onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks}/>
+    </div>
+  );
+}
+
+export default SelectedProject;
